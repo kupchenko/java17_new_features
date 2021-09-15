43e418c5a41ee02b9fac40fee4c75984290cd0e5
Dmitrii Kupchenko <kupchenko.work@gmail.com>
2021-09-15 21:42:49 +0300

    Add string blocks test

---
+++ src/main/java/me/kupchenko/TextBlocks.java
@@ -0,0 +1,43 @@
+package me.kupchenko;
+
+/**
+ * Goals
+ * <p>
+ * Simplify the task of writing Java programs by making it easy to express strings that span several lines
+ * of source code, while avoiding escape sequences in common cases.
+ * <p>
+ * Enhance the readability of strings in Java programs that denote code written in non-Java languages.
+ * <p>
+ * Support migration from string literals by stipulating that any new construct can express the same set
+ * of strings as a string literal, interpret the same escape sequences,
+ * and be manipulated in the same ways as a string literal.
+ * <p>
+ * Add escape sequences for managing explicit white space and newline control.
+ * <p>
+ * More details: https://openjdk.java.net/jeps/378
+ */
+public class TextBlocks {
+    String old_ugly_query = "SELECT \"EMP_ID\", \"LAST_NAME\" FROM \"EMPLOYEE_TB\"\n" +
+            "WHERE \"CITY\" = 'INDIANAPOLIS'\n" +
+            "ORDER BY \"EMP_ID\", \"LAST_NAME\";\n";
+
+    String query = """
+            SELECT "EMP_ID", "LAST_NAME" FROM "EMPLOYEE_TB"
+            WHERE "CITY" = 'INDIANAPOLIS'
+            ORDER BY "EMP_ID", "LAST_NAME";
+            """;
+
+    String old_ugly_html = "<html>\n" +
+            "    <body>\n" +
+            "        <p>Hello, world</p>\n" +
+            "    </body>\n" +
+            "</html>\n";
+
+    String html = """
+            <html>
+                <body>
+                    <p>Hello, world</p>
+                </body>
+            </html>
+            """;
+}
